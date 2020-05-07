5b5168dcc83895b89b26ce6f8d8f33aa33b13aa0
XLR8-07 <joyashikur@gmail.com>
2020-05-08 00:07:16 +0600

    Selector Form Now uses Material Form

--- FireStoreTest/src/app/employees/employees.component.ts
+++ FireStoreTest/src/app/employees/employees.component.ts
@@ -13,5 +13,11 @@ export class EmployeesComponent implements OnInit {
   ngOnInit(): void {
   }
  
+  locator(){
+    if(this.service.ButtonValue == true){
+      return '0em';
+    }
+    return 'auto';
+  }
 
 }
